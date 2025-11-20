6b691a1a6f5a122e121d623d233de3d175bf3990
Antoine Duchesne <antduchesne@hotmail.fr>
2025-11-20 16:54:23 +0100

    :art: TEM-26 Feed Utilisateur #STAGING

---
+++ src/Repository/PostRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Post;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Post>
+ */
+class PostRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Post::class);
+    }
+
+    /** @return Post[] */
+    public function getPaginatedPosts(int $offset, int $limit): array
+    {
+        $queryBuilder = $this->createQueryBuilder('p')
+            ->orderBy('p.createdAt', 'DESC')
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * @param array<int> $followedUserIds
+     *
+     * @return Post[]
+     */
+    public function getFollowedPaginatedPosts(array $followedUserIds, int $offset, int $limit): array
+    {
+        $queryBuilder = $this->createQueryBuilder('p')
+            ->where('p.userId IN (:followedUserIds)')
+            ->setParameter('followedUserIds', $followedUserIds)
+            ->orderBy('p.createdAt', 'DESC')
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+}
