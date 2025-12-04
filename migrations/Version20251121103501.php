53a4cbc7b1e788d4983f56dccd42a19df8be5427
timothee.lebeau <timothee.lebeau@epitech.eu>
2025-12-04 14:31:21 +0100

    :ambulance: modify migration

--- migrations/Version20251121103501.php
+++ migrations/Version20251121103501.php
@@ -16,32 +16,27 @@ final class Version20251121103501 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE conversation ADD is_group BOOLEAN DEFAULT false NOT NULL');
-        $this->addSql('ALTER TABLE conversation ADD group_name VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE conversation DROP type');
-        $this->addSql('ALTER TABLE conversation_participant DROP CONSTRAINT FK_398016619AC0396');
-        $this->addSql('ALTER TABLE conversation_participant DROP CONSTRAINT conversation_participant_pkey');
-        $this->addSql('ALTER TABLE conversation_participant ADD id SERIAL NOT NULL');
-        $this->addSql('ALTER TABLE conversation_participant ADD joined_at TIMESTAMP(0) WITHOUT TIME ZONE');
-        $this->addSql('ALTER TABLE conversation_participant ADD left_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+        $this->addSql('ALTER TABLE group_message DROP CONSTRAINT FK_30BD6473F675F31B');
+        $this->addSql('DROP TABLE group_message');
+        $this->addSql('CREATE TABLE conversation (id SERIAL NOT NULL, is_group BOOLEAN DEFAULT false NOT NULL, group_name VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN conversation.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN conversation.updated_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE conversation_participant (id SERIAL NOT NULL, conversation_id INT NOT NULL, user_id INT NOT NULL, role VARCHAR(20) DEFAULT \'member\' NOT NULL, joined_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, left_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_398016619AC0396 ON conversation_participant (conversation_id)');
+        $this->addSql('CREATE INDEX IDX_39801661A76ED395 ON conversation_participant (user_id)');
         $this->addSql('COMMENT ON COLUMN conversation_participant.joined_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN conversation_participant.left_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('ALTER TABLE conversation_participant ADD CONSTRAINT FK_398016619AC0396 FOREIGN KEY (conversation_id) REFERENCES conversation (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE conversation_participant ADD PRIMARY KEY (id)');
+        $this->addSql('ALTER TABLE conversation_participant ADD CONSTRAINT FK_39801661A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F9AC0396 FOREIGN KEY (conversation_id) REFERENCES conversation (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE conversation_participant DROP CONSTRAINT fk_398016619ac0396');
-        $this->addSql('DROP INDEX conversation_participant_pkey');
-        $this->addSql('ALTER TABLE conversation_participant DROP id');
-        $this->addSql('ALTER TABLE conversation_participant DROP joined_at');
-        $this->addSql('ALTER TABLE conversation_participant DROP left_at');
-        $this->addSql('ALTER TABLE conversation_participant ADD CONSTRAINT fk_398016619ac0396 FOREIGN KEY (conversation_id) REFERENCES conversation (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE conversation_participant ADD PRIMARY KEY (conversation_id, user_id)');
-        $this->addSql('ALTER TABLE conversation ADD type VARCHAR(20) NOT NULL');
-        $this->addSql('ALTER TABLE conversation DROP is_group');
-        $this->addSql('ALTER TABLE conversation DROP group_name');
+        $this->addSql('ALTER TABLE message DROP CONSTRAINT FK_B6BD307F9AC0396');
+        $this->addSql('ALTER TABLE conversation_participant DROP CONSTRAINT FK_398016619AC0396');
+        $this->addSql('ALTER TABLE conversation_participant DROP CONSTRAINT FK_39801661A76ED395');
+        $this->addSql('DROP TABLE conversation');
+        $this->addSql('DROP TABLE conversation_participant');
     }
 }
