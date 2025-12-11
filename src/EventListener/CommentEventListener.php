5d400d1b73483eeab8d966075692884d0c8fb1b0
Sergio <sergio.guedess@hotmail.com>
2025-12-11 12:29:29 +0100

    :sparkles: TEM-28 Creation comment & comment_count in post and adding the like for comment #staging

--- src/EventListener/CommentEventListener.php
+++ src/EventListener/CommentEventListener.php
@@ -8,9 +8,16 @@ use App\Entity\Comment;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Events;
 
+#[AsEntityListener(event: Events::postPersist, entity: Comment::class)]
 #[AsEntityListener(event: Events::preRemove, entity: Comment::class)]
 class CommentEventListener
 {
+    public function postPersist(Comment $comment): void
+    {
+        $post = $comment->getPost();
+        $post->incrementCommentsCount();
+    }
+
     public function preRemove(Comment $comment): void
     {
         $post = $comment->getPost();
