cb629464a59e94bb90973da8f63c2839375fe564
timothee.lebeau <timothee.lebeau@epitech.eu>
2026-05-22 15:37:50 +0200

    :ambulance: dont swallow doctrine errors

--- tests/State/Auth/AuthRequestProcessorUnitTest.php
+++ tests/State/Auth/AuthRequestProcessorUnitTest.php
@@ -43,7 +43,7 @@ class AuthRequestProcessorUnitTest extends TestCase
         $this->assertSame('If a matching email was found, a code has been sent.', $output->message);
     }
 
-    public function testLogsAndThrowsOnDatabaseError(): void
+    public function testPropagatesDatabaseError(): void
     {
         $repo = $this->createMock(EntityRepository::class);
         $repo->method('findOneBy')->willReturn(null);
@@ -53,17 +53,15 @@ class AuthRequestProcessorUnitTest extends TestCase
         $mockEm->method('flush')->willThrowException(new \Exception('Database error'));
 
         $mockLogger = $this->createMock(LoggerInterface::class);
-        $mockLogger->expects($this->once())
-            ->method('error')
-            ->with($this->stringContains('Could not process auth request: Database error'))
-        ;
+        $mockLogger->expects($this->never())->method('error');
 
         $mockMailer = $this->createMock(MailerInterface::class);
+        $mockMailer->expects($this->never())->method('send');
 
         $processor = new AuthRequestProcessor($mockEm, $mockMailer, $mockLogger);
 
-        $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('Could not process the request.');
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Database error');
 
         $processor->process(new AuthRequestInput('error@example.com'));
     }
